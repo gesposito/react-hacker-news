306636289847093718c30af6c9f8dcf810a5aaf0
Gianluca Esposito <g@esposi.to>
2019-06-09 12:35:20 +0200

    Updates

---
+++ precache-manifest.12451dce73008eda7a3d94f57c3ecbff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6fb00ea41ab2d28b1c98b9e888081b",
+    "url": "/react-hacker-news/index.html"
+  },
+  {
+    "revision": "d4658e5f415f7264ddb4",
+    "url": "/react-hacker-news/static/js/2.3a75199f.chunk.js"
+  },
+  {
+    "revision": "cc2572f58dfd8c1ae4d2",
+    "url": "/react-hacker-news/static/js/3.4bc39f28.chunk.js"
+  },
+  {
+    "revision": "3c0a76a877ad22aa7072",
+    "url": "/react-hacker-news/static/js/4.56991425.chunk.js"
+  },
+  {
+    "revision": "3b2a1c984050fbd17dcd",
+    "url": "/react-hacker-news/static/js/main.985db02a.chunk.js"
+  },
+  {
+    "revision": "5e4db510887d7ce9fac9",
+    "url": "/react-hacker-news/static/js/runtime~main.87a7f907.js"
+  }
+]);
